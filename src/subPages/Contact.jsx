58372d45723a8d34eae911b02bb54fc8afc5164a
fahadiqbal-01 <fahad.iqbal.im.47@gmail.com>
2026-04-23 10:47:49 +0600

    bit edited

--- src/subPages/Contact.jsx
+++ src/subPages/Contact.jsx
@@ -23,10 +23,10 @@ export default function Contact({ id }) {
   return (
     <div
       id={id}
-      className=" 2xl:my-20 xl:my-0 lg:my-20 md:my-10 my-10 2xl:pt-20 xl:pt-20 lg:pt-0 md:pt-10 pt-0 text-white selection:bg-red-600 selection:text-white font-sans overflow-x-hidden"
+      className=" 2xl:my-20 xl:my-20 lg:my-20 md:my-10 my-10 2xl:pt-20 xl:pt-20 lg:pt-0 md:pt-10 pt-10 text-white selection:bg-red-600 selection:text-white font-sans overflow-x-hidden"
     >
       <Container className="  ">
-        <div className="relative mb-10 md:mb-10 lg:mb-32 xl:mb-32 2xl:mb-32 text-center">
+        <div className="relative mb-10 md:mb-10 lg:mb-20 xl:mb-18 2xl:mb-20 text-center">
           <motion.h1
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
@@ -45,63 +45,7 @@ export default function Contact({ id }) {
           </motion.span>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-16 lg:gap-24">
-          {/* Left Column: Info */}
-          <motion.div
-            initial={{ opacity: 0, x: -30 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            viewport={{ once: true }}
-            className="lg:col-span-5 space-y-12"
-          >
-            <div className="space-y-6">
-              <p className="text-white/50  font-satoshi text-lg lg:text-xl leading-relaxed max-w-md">
-                At Drago, we blend strategy, design, and technology to craft
-                stunning websites, compelling brands, and seamless user
-                experiences that drive success.
-              </p>
-            </div>
-
-            <div className="pt-12 border-t border-white/10">
-              <h3 className="text-2xl font-satoshi mb-10 tracking-widest uppercase">
-                Contact Details
-              </h3>
-
-              <div className="grid grid-cols-1 sm:grid-cols-2 2xl:gap-10 xl:gap-10 lg:gap-10 md:gap-8 gap-6 ">
-                <div className="space-y-2">
-                  <p className="text-xs uppercase tracking-widest text-gray-500 font-satoshi">
-                    Phone/WhatsApp:
-                  </p>
-                  <a
-                    href="tel:+441234567890"
-                    className="text-lg hover:text-red-500 transition-colors duration-300"
-                  >
-                    +880 1735 035182
-                  </a>
-                </div>
-
-                <div className="space-y-2">
-                  <p className="text-xs uppercase tracking-widest text-gray-500 font-satoshi">
-                    Email:
-                  </p>
-                  <a
-                    href="mailto:hello@gmail.com"
-                    className="text-lg hover:text-red-500 transition-colors duration-300"
-                  >
-                    tanvirrahman2111@gmail.com Tools
-                  </a>
-                </div>
-
-                <div className="sm:col-span-2 space-y-2">
-                  <p className="text-xs uppercase tracking-widest text-gray-500 font-satoshi">
-                    Based In:
-                  </p>
-                  <p className="text-lg text-gray-300 leading-snug">
-                    Bangladesh
-                  </p>
-                </div>
-              </div>
-            </div>
-          </motion.div>
+        <div>
 
           <motion.div
             initial={{ opacity: 0, x: 30 }}
@@ -109,7 +53,8 @@ export default function Contact({ id }) {
             viewport={{ once: true }}
             className="lg:col-span-7"
           >
-            <form onSubmit={handleSubmit} className="space-y-8">
+            
+            <form onSubmit={handleSubmit} className="space-y-4">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div className="space-y-3">
                   <label className="text-xs font-satoshi uppercase tracking-widest text-white/50 ">
@@ -189,7 +134,7 @@ export default function Contact({ id }) {
               </div>
 
               {/* Footer Buttons */}
-              <div className="flex flex-col sm:flex-row items-center gap-8 pt-6">
+              <div className="flex flex-col sm:flex-row items-center gap-8">
                 <button
                   type="submit"
                   className="w-full sm:w-auto bg-white text-black font-patung uppercase tracking-widest px-12 py-4 rounded-lg cursor-pointer select-none
